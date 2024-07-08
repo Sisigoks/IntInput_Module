6e89e15ff0ba6d2a59567964e373d05bbd9d8989
Sisigoks <Sisi.goks2008@gmail.com>
2024-07-08 19:06:21 +0530

    Intinput development

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from IntInput import intinput
--- setup.py
+++ setup.py
@@ -7,19 +7,23 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.13'
+VERSION = '1.0.0'
 DESCRIPTION = 'This repository contains a Python function named `intinput` which prompts the user to input an integer value.'
 
 # Setting up
 setup(
-    name="vidstream",
+    name="intinput",
     version=VERSION,
     author="Sibi Gokul",
     author_email="<Sisi.goks2008@gmail.com>",
     description=DESCRIPTION,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[],
+    extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2"],
+    },
     keywords=['intinput'],
     classifiers=[
         "Development Status :: 1 - Planning",
